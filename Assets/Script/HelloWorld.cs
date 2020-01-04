6142ca40261c690252019118244405cb75f1da8b
Harish Rao <harish.rao@nike.com>
2020-01-04 10:43:52 -0800

    Added hello world
    From class 1

---
+++ Assets/Script/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello world 1 from start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.LogError("hello world 1 from update");
+    }
+}
